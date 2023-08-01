a2dc494d30d54deb3602dd4145a6606fcca4c00a
Chamaththa Shamod <shamodchamaththa@gmail.com>
2023-08-01 19:16:21 +0530

    Home Page UI

--- app/src/main/java/com/example/digitmaster/MainActivity2.kt
+++ app/src/main/java/com/example/digitmaster/MainActivity2.kt
@@ -4,10 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
 
-    private lateinit var start: Button
+    private lateinit var start: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
